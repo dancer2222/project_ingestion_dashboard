c450c1ad6b5aab318a4cc0fdfa33ad666a8b3be3
serhii-cherednyk <sergey.cherednik@hyuna.bb>
2019-04-11 19:48:43 +0300

    Vue search

---
+++ resources/js/components/Media/Books.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                Books header
+            </div>
+
+            <div class="card-body">
+                Books list
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'books',
+    data: function () {
+        return {
+            mediaType: 'books',
+            movies: [],
+        }
+    },
+    methods: {
+
+    },
+}
+</script>
---
+++ resources/js/components/Media/Search/Routes.js
@@ -0,0 +1,7 @@
+import Movies from './../Movies'
+import Books from './../Books'
+
+export default [
+  { path: '/movies', component: Movies },
+  { path: '/books', component: Books },
+]
