a8480e69542a72b1e3cfa156c67c5c937921a99d
slavab-hyuna <slava.bedovsky@hyuna.bb>
2019-05-20 16:31:24 +0300

    Added button description book

--- resources/js/components/Media/Books/Book.vue
+++ resources/js/components/Media/Books/Book.vue
@@ -26,12 +26,22 @@
 
                         <button class="btn btn-outline-info float-right" v-on:click="editBook">edit</button>
                         <div >
-                        <img v-if="book.num_of_images > 0" v-bind:src="book.coverUrl" alt="Book cover"/>
-                    </div>
+                            <img v-if="book.num_of_images > 0" v-bind:src="book.coverUrl" alt="Book cover"/>
+                            <img v-else v-bind:src="pictureError" v-bind:width="200" v-bind:height="200" alt="No picture available">
+                        </div>
                         <div class="row">
 
                             <p class="card-title mb-2 col-12 col-md-6 col-lg-4" v-if="book" v-for="(value, key) in book">
-                                <b>{{ key }}</b> - {{ value }}
+
+                                <b v-if="key == 'description'">
+                                    <button @click="descriptionShow=!descriptionShow" class="btn btn-sm btn-outline-dark">{{key}}</button>
+                                    <div v-show="descriptionShow">
+                                        <b>{{ value }}</b>
+                                    </div>
+                                </b>
+
+                                <b v-if="key !== 'seq_id' && key !== 'description'">{{ key }} - {{ value }}</b>
+
                             </p>
                         </div>
                     </div>
@@ -54,6 +64,8 @@
                 errorMessage: 'Something went wrong. Please try reloading the <a href="javascript:location.reload();">page</a>',
                 book: {},
                 mediaType: 'books',
+                descriptionShow : false,
+                pictureError: 'img/image-not-found.jpeg',
             }
         },
         methods: {
