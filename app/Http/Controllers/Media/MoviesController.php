832208302e01d6b644ee326d60020b678695dccf
oleksandrd-hyuna <oleksandr.danileyko@hyuna.bb>
2019-07-18 18:48:17 +0300

    fix movie info

--- app/Http/Controllers/Media/MoviesController.php
+++ app/Http/Controllers/Media/MoviesController.php
@@ -60,7 +60,7 @@ class MoviesController extends Controller
             'producers:id,name',
             'directors:id,name',
             'writers:id,name,status',
-            'brightcoveId:id,brightcove_Id,status,brightcove_trailer_id,non_drm_brightcove_id'
+            'brightcoveId:id,brightcove_Id,status,non_drm_brightcove_id'
         ])->first();
 
         if($movie && $movie->num_of_images > 0){
