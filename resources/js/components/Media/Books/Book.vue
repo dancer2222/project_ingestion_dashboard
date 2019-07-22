e07441e1f7b4876d8e2a61f043444f3b76fa7867
yurijl-hyuna <yurij.leshinskyy@hyuna.bb>
2019-07-22 12:18:09 +0300

    redesign search

--- resources/js/components/Media/Books/Book.vue
+++ resources/js/components/Media/Books/Book.vue
@@ -1,103 +1,260 @@
 <template>
-    <div class="col-12">
-        <div class="row">
+	<div class="col-12">
+		<div class="row">
+			<div class="col-12 col-sm-12">
+				<div class="alert alert-danger" role="alert" v-if="isError" v-html="errorMessage"></div>
 
-            <div class="col-12 col-sm-12">
-                <div class="alert alert-danger" role="alert" v-if="isError" v-html="errorMessage"></div>
+				<div class="card">
+					<div class="card-header">
+						<button class="btn btn-outline-dark float-left mr-3" v-on:click="$router.back()">Back</button>
 
-                <div class="card">
-                    <div class="card-header">
-                        <button class="btn btn-outline-dark float-left mr-3" v-on:click="$router.back()">Back</button>
+						<strong>Book</strong>
+						<span v-if="book.isbn">ISBN:{{book.isbn}}</span>
+						<span v-else>{{book.title}}</span>
 
-                        <strong>Book</strong>
-                        <span v-if="book.isbn">ISBN:{{book.isbn}}</span>
-                        <span v-else>{{book.title}}</span>
+						<button class="btn btn-outline-info float-right" v-on:click="editBook">edit</button>
+					</div>
 
-                        <div class = 'float-right'>
-                            <span><strong >Status:</strong></span>
-                            <span v-if="book" v-bind:class="['badge', book.status === 'active' ? 'badge-success' : 'badge-danger']">{{ book.status }}</span>
-                        </div>
+					<div class="card-body" v-if="book">
+						<div class="row">
+							<div class="card_info_wrapper col-sm-12 col-lg-6">
+								<h5 class="mb-4">
+									<b>
+										Show on prodution this book -
+										<a href="#" target="_blank">{{ book.title }}</a>
+									</b>
+								</h5>
 
-                    </div>
-                    <!--                    <img class="card-img-top mx-auto" src="img/no_product_image.png"-->
-                    <!--                         v-bind:style="{maxWidth: 250 + 'px'}" alt="Card image cap">-->
+								<div class="card border-success" v-bind:class="[book.status == 'active' ? 'border-success' : 'border-danger']">
+									<div class="row" v-if="book">
+										<div class="image_wrapper">
+											<span class="status" v-bind:class="[book.status == 'active' ? 'badge-success' : 'badge-danger']">
+												{{book.status}}
+											</span>
+											<img v-if="book.num_of_images > 0"
+												v-bind:src="book.coverUrl"
+												alt="Book cover"
+											/>
+											<img v-else
+												v-bind:src="pictureError"
+												v-bind:width="200"
+												v-bind:height="200"
+												alt="No picture available"
+											/>
+										</div>
+										<p class="card-title col-12">
+											<button @click="zeroShow=!zeroShow" class="btn btn-sm btn-outline-dark" >
+												{{ zeroShow ? "Hide null values" : "Show null values" }}
+											</button>
+											<button @click="descriptionShow=!descriptionShow" class="btn btn-sm btn-outline-dark" >
+												description
+											</button>
+										</p>
+										<p class="card-title col-12">
+											<b>
+												<div v-show="descriptionShow">
+													{{ book.description }}
+												</div>
+											</b>
+										</p>
+										<p class="card-title col-12" v-for="(value, title) in book">
 
-                    <div class="card-body" v-if="book">
+											<b v-show="zeroShow" 
+												v-if="value == 0 && 
+													value == '' &&
+													value !== null">
+												<span class="title">{{ title }}</span> - {{ value }}
+											</b>
 
-                        <button class="btn btn-outline-info float-right" v-on:click="editBook">edit</button>
-                        <div >
-                            <img v-if="book.num_of_images > 0" v-bind:src="pictureURL" alt="Book cover"/>
-                            <img v-else v-bind:src="pictureError" v-bind:width="200" v-bind:height="200" alt="No picture available">
-                        </div>
-                        <div class="row">
+											<b v-if="title !== 'seq_id' && 
+													title !== 'description' && 
+													title !== 'licensor' && 
+													value !== 0 && 
+													value !== '' && 
+													value !== null && 
+													!Array.isArray(value)">
+												<span class="title">{{ title }}</span> - {{ value }}
+											</b>
 
-                            <p class="card-title mb-2 col-12 col-md-6 col-lg-4" v-if="book" v-for="(value, key) in book">
+											<b v-if="title == 'licensor'">
+												<span class="title">{{ title }}</span>: {{ value.name }} [{{ value.id }}]
+												<span class="green">{{ value.status }}</span>
+											</b>
 
-                                <b v-if="key == 'description'">
-                                    <button @click="descriptionShow=!descriptionShow" class="btn btn-sm btn-outline-dark">{{key}}</button>
-                                    <div v-show="descriptionShow">
-                                        <b>{{ value }}</b>
-                                    </div>
-                                </b>
+											<b v-if="Array.isArray(value) && title !== 'publisher' && title !== 'artists'">
+												<span class="title">{{ title }}</span>:
+												<span v-for="val in value">
+													{{ val.name }} [{{ val.id }}] <span class="green">{{ val.status }}</span>
+													<br />
+												</span>
+											</b>
+										</p>
+									</div>
+								</div>
+							</div>
 
-                                <b v-if="key !== 'seq_id' && key !== 'description' && key !== 'author'">{{ key }} - {{ value }}</b>
+							<div class="card_tabs_wrapper col-sm-12 col-lg-6">
+								<h5 class="mb-4">
+									<b>
+										Show on QA this book -
+										<a href="#" target="_blank">{{ book.title }}</a>
+									</b>
+								</h5>
 
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+								<div class="card">
+									<div class="card-body p-b-0">
+										<ul class="nav nav-tabs customtab2" role="tablist">
+											<li class="nav-item">
+												<a class="nav-link active show"
+													data-toggle="tab"
+													href="#georestricts"
+													role="tab"
+													aria-selected="true"
+												>
+													<span class="hidden-xs-down">Geo Restricts</span>
+												</a>
+											</li>
 
-        </div>
-    </div>
+											<li class="nav-item">
+												<a class="nav-link show"
+													data-toggle="tab"
+													href="#siterestricts"
+													role="tab"
+													aria-selected="false"
+												>
+													<span class="hidden-xs-down">Site Restricts</span>
+												</a>
+											</li>
+
+											<li class="nav-item">
+												<a class="nav-link"
+													data-toggle="tab"
+													href="#status_info"
+													role="tab"
+													aria-selected="false"
+												>
+													<span class="hidden-xs-down">Status info</span>
+												</a>
+											</li>
+
+											<li class="nav-item hidden">
+												<a class="nav-link"
+													data-toggle="tab"
+													href="#failed_items"
+													role="tab"
+													aria-selected="false"
+												>
+													<span class="hidden-xs-down">Failed items</span>
+												</a>
+											</li>
+										</ul>
+
+										<div class="tab-content">
+											<div class="tab-pane active show" id="georestricts" role="tabpanel">
+												<b>Active --</b>
+												<span class="text-secondary">UM</span>
+												<span class="text-danger font-weight-bold">US</span>
+
+												<hr />
+
+												<b>Inactive --</b>
+											</div>
+											<div class="tab-pane" id="status_info" role="tabpanel">
+												<table class="table table-hover">
+													<thead class="thead-dark">
+														<tr>
+														<th scope="col">#</th>
+														<th scope="col">From</th>
+														<th scope="col">To</th>
+														<th scope="col">Date added</th>
+														</tr>
+													</thead>
+
+													<tbody>
+														<tr>
+														<th scope="row">38391626</th>
+														<td>inactive</td>
+														<td>active</td>
+														<td>2017-06-30 13:42:57</td>
+														</tr>
+														
+														<tr>
+														<th scope="row">38380697</th>
+														<td>active</td>
+														<td>inactive</td>
+														<td>2017-06-30 12:45:04</td>
+														</tr>
+													</tbody>
+												</table>
+											</div>
+											<div class="tab-pane" id="failed_items" role="tabpanel">
+												<table class="table table-hover">
+													<tbody></tbody>
+												</table>
+											</div>
+											<div class="tab-pane" id="siterestricts" role="tabpanel">
+												<table class="table table-hover">
+													<tbody></tbody>
+												</table>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-    import axios from 'axios'
+import axios from "axios";
 
-    export default {
-        debug: true,
-        name: 'booksDataView',
-        data: function () {
-            return {
-                isError: false,
-                errorMessage: 'Something went wrong. Please try reloading the <a href="javascript:location.reload();">page</a>',
-                book: {},
-                mediaType: 'books',
-                descriptionShow : false,
-                pictureError: 'img/image-not-found.jpeg',
-                pictureURL:'',
-            }
-        },
-        methods: {
-            fetchData: function () {
-                let self = this;
-                axios.get(this.mediaType + '/' + this.$route.params.id)
-                    .then(function (response) {
-                        if (Object.keys(response.data).length > 0) {
-                            self.book = response.data;
-                            self.pictureURL = self.book.coverUrl;
-                            Vue.delete(self.book, 'coverUrl');
-                        } else {
-                            self.isError = true;
-                        }
-                    }).catch(function (error) {
-                    self.isError = true;
-                })
-            },
-            editBook: function (e) {
-                this.$router.push({
-                    name: 'books_edit',
-                    path: 'book',
-                    params: { book: this.book},
-                })
-            },
-        },
-        mounted() {
-            console.log('book component');
-            this.fetchData();
-        },
-
-    }
-
+export default {
+	debug: true,
+	name: "booksDataView",
+	data: function() {
+		return {
+			isError: false,
+			errorMessage:
+				'Something went wrong. Please try reloading the <a href="javascript:location.reload();">page</a>',
+			book: {},
+			mediaType: "books",
+			descriptionShow: false,
+			zeroShow: false,
+			pictureError: "img/image-not-found.jpeg",
+			pictureURL: ""
+		};
+	},
+	methods: {
+		fetchData: function() {
+			let self = this;
+			axios.get(this.mediaType + "/" + this.$route.params.id)
+				.then(function(response) {
+					if (Object.keys(response.data).length > 0) {
+						self.book = response.data;
+						self.pictureURL = self.book.coverUrl;
+						Vue.delete(self.book, "coverUrl");
+					} else {
+						self.isError = true;
+					}
+				}).catch(function(error) {
+					self.isError = true;
+			});
+		},
+		editBook: function(e) {
+			this.$router.push({
+				name: "books_edit",
+				path: "book",
+				params: { book: this.book }
+			});
+		}
+	},
+	mounted() {
+		console.log("book component");
+		this.fetchData();
+	}
+};
 </script>
--- resources/js/components/Media/Movie.vue
+++ resources/js/components/Media/Movie.vue
@@ -1,92 +1,250 @@
 <template>
-    <div class="col-12">
-        <div class="row">
+	<div class="col-12">
+		<div class="row">
+			<div class="col-12 col-sm-12">
+				<div class="alert alert-danger" role="alert" v-if="isError" v-html="errorMessage"></div>
 
-            <div class="col-12 col-sm-12">
-                <div class="alert alert-danger" role="alert" v-if="isError" v-html="errorMessage"></div>
+				<div class="card">
+					<div class="card-header">
+						<button class="btn btn-outline-dark float-left mr-3" v-on:click="$router.back()">Back</button>
 
-                <div class="card">
-                    <div class="card-header">
-                        <button class="btn btn-outline-dark float-left mr-3" v-on:click="$router.back()">Back</button>
+						{{ movie.title }}
+						
+						<button class="btn btn-outline-info float-right" v-on:click="editMovie">edit</button>
+					</div>
 
-                        {{ movie.title }}
+					<div class="card-body">
+						<div class="row">
+							<div class="card_info_wrapper col-sm-12 col-lg-6">
+								<h5 class="mb-4">
+									<b>
+										Show on prodution this movie -
+										<a href="#" target="_blank">{{ movie.title }}</a>
+									</b>
+								</h5>
 
-                        <button class="btn btn-outline-info float-right" v-on:click="editMovie">edit</button>
+								<div class="card border-success" v-bind:class="[movie.status == 'active' ? 'border-success' : 'border-danger']">
+									<div class="row" v-if="movie">
+										<div class="image_wrapper">
+											<span class="status" v-bind:class="[movie.status == 'active' ? 'badge-success' : 'badge-danger']">
+												{{movie.status}}
+											</span>
+											<img
+												v-if="movie.num_of_images > 0"
+												v-bind:src="movie.coverUrl"
+												alt="Movie cover"
+											/>
+											<img
+												v-else
+												v-bind:src="pictureError"
+												v-bind:width="200"
+												v-bind:height="200"
+												alt="No picture available"
+											/>
+										</div>
+										<p class="card-title col-12">
+											<button @click="zeroShow=!zeroShow" class="btn btn-sm btn-outline-dark" >
+												{{ zeroShow ? "Hide null values" : "Show null values" }}
+											</button>
+											<button @click="descriptionShow=!descriptionShow" class="btn btn-sm btn-outline-dark" >
+												description
+											</button>
+										</p>
+										<p class="card-title col-12">
+											<b>
+												<div v-show="descriptionShow">
+													{{ movie.description }}
+												</div>
+											</b>
+										</p>
+										<p class="card-title col-12" v-for="(value, title) in movie">
+											<b v-show="zeroShow"
+												v-if="value == 0 && 
+												value == '' &&
+												value !== null">
+												<span class="title">{{ title }}</span> - {{ value }}
+											</b>
 
-                    </div>
+											<b v-if="title !== 'seq_id' && 
+												title !== 'description' && 
+												title !== 'licensor' && 
+												value !== 0 && 
+												value !== '' && 
+												value !== null && 
+												!Array.isArray(value)">
+												<span class="title">{{ title }}</span> - {{ value }}
+											</b>
 
-                    <div class="card-body">
-                        <div>
-                            <img v-if="movie.num_of_images > 0" v-bind:src="movie.coverUrl" alt="Movie cover"/>
-                            <img v-else v-bind:src="pictureError" v-bind:width="200" v-bind:height="200"
-                                 alt="No picture available">
-                        </div>
-                        <div class="row">
-                            <p class="card-title mb-2 col-12 col-md-6 col-lg-4" v-if="movie"
-                               v-for="(value, key) in movie">
+											<b v-if="title == 'licensor'">
+												<span class="title">{{ title }}</span>: {{ value.name }} [{{ value.id }}] <span class="green">{{ value.status }}</span>
+											</b>
 
-                                <b v-if="key == 'description'">
-                                    <button @click="descriptionShow=!descriptionShow"
-                                            class="btn btn-sm btn-outline-dark">{{key}}
-                                    </button>
-                                    <div v-show="descriptionShow">
-                                        <b>{{ value }}</b>
-                                    </div>
-                                </b>
+											<b v-if="Array.isArray(value) && value.length > 0">
+												<span class="title">{{ title }}</span>:
+												<span v-for="val in value">
+													{{ val.name }} [{{ val.id }}] <span class="green">{{ val.status }}</span> 
+													<br/>
+												</span>
+											</b>
+										</p>
+									</div>
+								</div>
+							</div>
 
-                                <b v-if="key !== 'seq_id' && key !== 'description'">{{ key }} - {{ value }}</b>
+							<div class="card_tabs_wrapper col-sm-12 col-lg-6">
+								<h5 class="mb-4">
+									<b>
+										Show on QA this movie -
+										<a href="#" target="_blank">{{ movie.title }}</a>
+									</b>
+								</h5>
 
-                            </p>
-                        </div>
-                    </div>
+								<div class="card">
+									<div class="card-body p-b-0">
+										<ul class="nav nav-tabs customtab2" role="tablist">
+											<li class="nav-item">
+												<a class="nav-link active show"
+													data-toggle="tab"
+													href="#georestricts"
+													role="tab"
+													aria-selected="true">
+													<span class="hidden-xs-down">Geo Restricts</span>
+												</a>
+											</li>
 
-                </div>
-            </div>
-        </div>
-    </div>
+											<li class="nav-item">
+												<a class="nav-link show"
+													data-toggle="tab"
+													href="#siterestricts"
+													role="tab"
+													aria-selected="false">
+													<span class="hidden-xs-down">Site Restricts</span>
+												</a>
+											</li>
+
+											<li class="nav-item">
+												<a class="nav-link"
+													data-toggle="tab"
+													href="#status_info"
+													role="tab"
+													aria-selected="false">
+													<span class="hidden-xs-down">Status info</span>
+												</a>
+											</li>
+
+											<li class="nav-item hidden">
+												<a class="nav-link"
+													data-toggle="tab"
+													href="#failed_items"
+													role="tab"
+													aria-selected="false">
+													<span class="hidden-xs-down">Failed items</span>
+												</a>
+											</li>
+										</ul>
+
+										<div class="tab-content">
+											<div class="tab-pane active show" id="georestricts" role="tabpanel">
+												<b>Active --</b>
+												<span class="text-secondary">UM</span>
+												<span class="text-danger font-weight-bold">US</span>
+
+												<hr />
+
+												<b>Inactive --</b>
+											</div>
+											<div class="tab-pane" id="status_info" role="tabpanel">
+												<table class="table table-hover">
+													<thead class="thead-dark">
+														<tr>
+															<th scope="col">#</th>
+															<th scope="col">From</th>
+															<th scope="col">To</th>
+															<th scope="col">Date added</th>
+														</tr>
+													</thead>
+
+													<tbody>
+														<tr>
+															<th scope="row">38391626</th>
+															<td>inactive</td>
+															<td>active</td>
+															<td>2017-06-30 13:42:57</td>
+														</tr>
+
+														<tr>
+															<th scope="row">38380697</th>
+															<td>active</td>
+															<td>inactive</td>
+															<td>2017-06-30 12:45:04</td>
+														</tr>
+													</tbody>
+												</table>
+											</div>
+											<div class="tab-pane" id="failed_items" role="tabpanel">
+												<table class="table table-hover">
+													<tbody></tbody>
+												</table>
+											</div>
+											<div class="tab-pane" id="siterestricts" role="tabpanel">
+												<table class="table table-hover">
+													<tbody></tbody>
+												</table>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-    import axios from 'axios';
+import axios from "axios";
 
-    export default {
-        debug: true,
-        name: 'movie',
-        data: function () {
-            return {
-                isError: false,
-                errorMessage: 'Something went wrong. Please try reloading the <a href="javascript:location.reload();">page</a>',
-                movie: {},
-                descriptionShow: false,
-                pictureError: 'img/image-not-found.jpeg',
-            }
-        },
-        mounted() {
-            console.log('movie component');
-            this.fetchData();
-        },
-        methods: {
-            fetchData: function () {
-                let self = this;
+export default {
+	debug: true,
+	name: "movie",
+	data: function() {
+		return {
+			isError: false,
+			errorMessage: 'Something went wrong. Please try reloading the <a href="javascript:location.reload();">page</a>',
+			movie: {},
+			descriptionShow: false,
+			zeroShow: false,
+			pictureError: "img/image-not-found.jpeg"
+		};
+	},
+	mounted() {
+		console.log("movie component");
+		this.fetchData();
+	},
+	methods: {
+		fetchData: function() {
+			let self = this;
 
-                axios.get(location.origin + '/movies/' + this.$route.params.id)
-                    .then(function (response) {
-                        if (Object.keys(response.data).length > 0) {
-                            self.movie = response.data;
-                        } else {
-                            self.isError = true;
-                        }
-                    }).catch(function (error) {
-                    self.isError = true;
-                })
-            },
-            editMovie: function (e) {
-                this.$router.push({
-                    name: 'movie_edit',
-                    path: 'movie',
-                    params: {movie: this.movie, fetchData: this.fetchData},
-                })
-            },
-        },
-    }
+			axios.get(location.origin + "/movies/" + this.$route.params.id)
+				.then(function(response) {
+					if (Object.keys(response.data).length > 0) {
+						self.movie = response.data;
+					} else {
+						self.isError = true;
+					}
+				}).catch(function(error) {
+					self.isError = true;
+			});
+		},
+		editMovie: function(e) {
+			this.$router.push({
+				name: "movie_edit",
+				path: "movie",
+				params: { movie: this.movie, fetchData: this.fetchData }
+			});
+		}
+	}
+};
 </script>
